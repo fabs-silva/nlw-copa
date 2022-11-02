fdf5b3e20553810a700495ecf0abf0c1b9f8a093
Fabiana Silva <fabianabz@yahoo.com>
2022-11-02 10:24:27 -0300

    aula 1

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,17 @@
+import { Center, Text } from 'native-base';
+import React from 'react';
+
+export function SignIn() {
+	return (
+		<Center
+			flex={1}
+			bgColor="gray.900">
+			<Text
+				color="white"
+				fontSize={24}
+				fontFamily="heading">
+				SignIn
+			</Text>
+		</Center>
+	);
+}
